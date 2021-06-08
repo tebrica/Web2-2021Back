481379c0cd9456024efc4093bd9e85488c2e109b
nejvej@gmail.com <nejvej@gmail.com>
2021-06-08 18:42:02 +0000

    Added gitignore

---
+++ Web2ProjBack/Backend/Data/Migrations/20210608182418_migracija.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Backend.Data.Migrations
+{
+    public partial class migracija : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
