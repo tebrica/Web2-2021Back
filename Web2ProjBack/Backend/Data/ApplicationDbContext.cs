fc8f122f93184b34b25b50ede28cf653b3ce8dea
maka031 <mlazovic1998@gmail.com>
2021-06-08 21:39:14 +0200

    Dodavanje modela

--- Web2ProjBack/Backend/Data/ApplicationDbContext.cs
+++ Web2ProjBack/Backend/Data/ApplicationDbContext.cs
@@ -12,6 +12,13 @@ namespace Backend.Data
         public DbSet<Incident> Incidents { get; set; }
         public DbSet<Ekipa> Ekipe { get; set; }
         public DbSet<Element> Elementi { get; set; }
+        public DbSet<NalogRada> NaloziRada { get; set; }
+        public DbSet<PlanRada> PlanoviRada { get; set; }
+        public DbSet<Poruka> Poruke { get; set; }
+        public DbSet<Potrosac> Potrosaci { get; set; }
+        public DbSet<Poziv> Pozivi { get; set; }
+        public DbSet<Resolution> Resolutions { get; set; }
+        public DbSet<SafetyDocument> SafetyDocuments { get; set; }
 
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
             : base(options)
